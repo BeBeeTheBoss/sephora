d3ba0a123c850f5bbe1a2dcc9886e1d2c3fe41e7
BeBee <originalbebee@gmail.com>
2024-09-09 22:26:25 +0630

    db structure setup done

--- database/migrations/2024_09_09_151714_create_products_table.php
+++ database/migrations/2024_09_09_151714_create_products_table.php
@@ -11,13 +11,13 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('category_id')->constrained()->references('id')->on('categories')->onDelete('cascade');
             $table->string('name');
-            $table->string('phone')->nullable();
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->enum('role', ['admin', 'user'])->default('user');
+            $table->longText('description')->nullable();
+            $table->unsignedBigInteger('price');
+            $table->boolean('is_active')->default(1);
             $table->timestamps();
         });
     }
@@ -27,6 +27,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('products');
     }
 };
--- database/migrations/2024_09_09_152835_create_carts_table.php
+++ database/migrations/2024_09_09_152835_create_carts_table.php
@@ -11,13 +11,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('carts', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('phone')->nullable();
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->enum('role', ['admin', 'user'])->default('user');
+            $table->foreignId('user_id')->constrained()->references('id')->on('users')->onDelete('cascade');
+            $table->foreignId('product_id')->constrained()->references('id')->on('products')->onDelete('cascade');
+            $table->integer('quantity')->default(1);
             $table->timestamps();
         });
     }
@@ -27,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('carts');
     }
 };
